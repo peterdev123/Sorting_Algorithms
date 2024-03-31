2630e1c6ee12243bbf28d893251cacbd20758be1
Peter <xsterfo456@gmail.com>
2024-03-31 12:15:47 +0800

    Changed Values

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 # from SelectionSorting import SelectionSorting
 from InsertionSorting import InsertionSorting
 
-array = [21, 15, 8, 12, 102, 5, 3]
+array = [21, 15, 8.6, 8.5, 12, 102, 5, 3]
 
 # myClass = BubbleSorting(array)
 # myClass = SelectionSorting(array)
